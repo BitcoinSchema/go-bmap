6e6e17775551172b8157515ef164dcbd2942b260
Siggi <github@icellan.net>
2022-11-04 21:43:26 +0100

    Updated go-b to 0.0.16, added test

---
+++ test/testing.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+    "os"
+    "strings"
+)
+
+// GetTestHex gets the hex of the given test file
+func GetTestHex(fileName string) string {
+    fileData, err := os.ReadFile(fileName) //nolint:gosec // only used in testing
+    if err != nil {
+        return ""
+    }
+
+    return strings.Trim(string(fileData), "\n")
+}
