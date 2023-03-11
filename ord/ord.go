3be4195a47561c320404de0fb44a682b35c5cf58
Luke Rohenaz <rohenaz@gmail.com>
2023-03-11 17:17:45 -0500

    add ord prefix

--- ord/ord.go
+++ ord/ord.go
@@ -1,8 +1,9 @@
 package ord
 
+// Prefix is the OP_RETURN prefix for the 1Sat Ordinals inscription protocol
 const Prefix string = "ord"
 
-// Ordinal tells wether an token or inscription are found
+// Ordinal tells wether an inscription is found
 type Ordinal struct {
 	Inscription bool
 }
